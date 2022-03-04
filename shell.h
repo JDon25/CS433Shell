e236de913af816af25c7d038937bd3479e2a8da3
James Donaldson <98553251+JDon25@users.noreply.github.com>
2022-03-04 00:23:57 -0800

    Add files via upload

--- shell.h
+++ shell.h
@@ -1,3 +1,9 @@
+/*
+Assignment: 2 - Shell OS
+
+Name: Tyler Desharnais, James Donaldson
+Date: 8 March 2022
+  */
 #pragma once
 #include <iostream>
 #include <vector>
@@ -6,18 +12,27 @@
 using namespace std;
 
 const int MAXLINE = 80;
-
+//H-file to establish all the functions in shell.cpp
 class shell {
 
 public:
 	shell();
+  //variable for active and ampersand
   bool active;
   bool amper;
+  //function to display history
   void displayHist();
+  //function to parse the string to get the command
 	void parseline(char buf [], char* make []);
+  //function to execute the user command
 	void execCommUser(char* make[]);
+  //function to execute to the shell command 
   void ShellComm(char* make[]);
+  //function to check if entered command is a user command
   bool isUserComm (char* make[]);
+  //function to store into history
+  void storHis (char comm[]);
 private:
+  //vector for the history function of the shell
 	vector<string> history;
 };
